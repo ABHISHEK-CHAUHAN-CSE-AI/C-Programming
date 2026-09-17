0ccd02c47a51068d3580373539cafa1f8572a7d5
ABHISHEK-CHAUHAN-CSE-AI <abhishekchauhan01012010@gmail.com>
2026-09-17 18:34:57 +0530

    Create fibonacci.c
    Written a C program to generate the Fibonacci sequence up to N terms using iterative loop logic.

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+
+    printf("Kitne terms tak Fibonacci series print karni hai? ");
+    scanf("%d", &n);
+
+    printf("\n--- Fibonacci Series ---\n");
+
+    for (int i = 1; i <= n; ++i) {
+        printf("%d ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+
+    return 0;
+}
